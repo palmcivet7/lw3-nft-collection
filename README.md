087261170714357762aebf3904b2e9fed1001ee1
palmcivet7 <palmcivet@proton.me>
2023-04-09 15:32:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NFT Collection
+
+This project was made following along with a lw3 lesson. It includes a front-end and a back-end.
---
+++ back-end/constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xf36eaa7d54c30ce282cbbd9810b86c0acc3d6aba";
+const METADATA_URL = "https://lw3-whitelist-dapp-gamma.vercel.app/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };

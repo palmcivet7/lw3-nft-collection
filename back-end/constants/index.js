f551058c5c4665daeadf37c34a238c96f2c024a7
palmcivet7 <palmcivet@proton.me>
2023-04-09 15:48:59 +0100

    add api route

--- back-end/constants/index.js
+++ back-end/constants/index.js
@@ -1,4 +1,4 @@
-const WHITELIST_CONTRACT_ADDRESS = "0xf36eaa7d54c30ce282cbbd9810b86c0acc3d6aba";
-const METADATA_URL = "https://lw3-whitelist-dapp-gamma.vercel.app/";
+const WHITELIST_CONTRACT_ADDRESS = "0xd486A0E908582b25707DF2adc0A350E77D6c9f9b";
+const METADATA_URL = "https://lw3-nft-collection-rho.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
